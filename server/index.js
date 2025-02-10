ebf45f4f306e09bc5adc6973c999eeb59232455d
Nino <anthonydavidtabet@gmail.com>
2025-02-11 04:33:07 +0700

    created backend template

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const pool = require('./db');
+const cors = require('cors');
+const PORT = process.env.PORT;
+
+app.use(express.json(), cors());
+
+app.get('/Search', async (req, res) => {
+    try {
+        
+        return res.status(200).json();
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+                
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
+});
