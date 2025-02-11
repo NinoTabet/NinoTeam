c678053b01ecdb9093be00f7093bf90b327ddbfe
Nino <anthonydavidtabet@gmail.com>
2025-02-11 21:51:49 +0700

    installed tiktok-live-connector npm package

--- server/index.js
+++ server/index.js
@@ -7,16 +7,17 @@ const PORT = process.env.PORT;
 
 app.use(express.json(), cors());
 
-app.get('/Search', async (req, res) => {
-    try {
-        
-        return res.status(200).json();
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({ message: 'Internal Server Error' });
-    }
+/*
+    app.get('/Search', async (req, res) => {
+        try {
+            
+            return res.status(200).json();
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
 });
-                
+*/              
 app.listen(PORT || 3000, () => {
     console.log(`Server is running on port 3000`);
 });
